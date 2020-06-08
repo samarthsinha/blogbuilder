c13ac6f8ac68f45e5cfcba59f61ddb2f952f3834
Samarth Sinha <samarthsinha93@gmail.com>
2020-06-08 22:52:37 +0530

    Updates

---
+++ precache-manifest.badadbfcf6aada451a609fb8e98d4067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0ed034abc34e664d0d06d58918281c",
+    "url": "/blogbuilder/index.html"
+  },
+  {
+    "revision": "1be60767b5985c5c0a59",
+    "url": "/blogbuilder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1d1b60ba9ea14bf8f93",
+    "url": "/blogbuilder/static/js/2.01b76f2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blogbuilder/static/js/2.01b76f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be60767b5985c5c0a59",
+    "url": "/blogbuilder/static/js/main.f8beb588.chunk.js"
+  },
+  {
+    "revision": "19c4ba01d02db7ba0a14",
+    "url": "/blogbuilder/static/js/runtime-main.2cd07b03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blogbuilder/static/media/logo.5d5d9eef.svg"
+  }
+]);
