02fc6f324ef8b1c150a7dd728d63222b577e23eb
Samarth Sinha <samarthsinha93@gmail.com>
2020-06-10 00:27:49 +0530

    todo with redux

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -2,7 +2,7 @@ import React from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import VisibilityFilters from "./components/VisibilityFilters";
-import "./App.css";
+import "./styles.css";
 
 export default function TodoApp() {
   return (
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import rotateReducer from "reducers/rotateReducer";
+
+
+function configureStore(state = { rotating: true }) {
+  return createStore(rotateReducer,state);
+}
+
+export default configureStore;
